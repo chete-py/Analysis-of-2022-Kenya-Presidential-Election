4f00317bb4678cefc476245170d07e634a8c4929
chete-py <112887160+chete-py@users.noreply.github.com>
2022-09-15 09:06:17 +0300

    Update elections.py

--- elections.py
+++ elections.py
@@ -1,28 +1,12 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[454]:
-
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
 
-# In[455]:
-
-
 df = pd.read_excel("COLLINS PROJECT.xlsx")
 df.head()
 
-
-# In[456]:
-
-
 df.columns
-
-
-# In[457]:
 
 
 newdf = df[['COUNTY','RAILA', 'RUTO', 'MWAURE','WAJACKOYA', 'TOTAL VALID VOTES']]
@@ -30,37 +14,19 @@ newdf.dropna()
 newdf.head()
 
 
-# In[458]:
-
-
 ammended_df = newdf.assign(RAILA_PERCENT =newdf['RAILA']/newdf['TOTAL VALID VOTES']*100, RUTO_PERCENT =newdf['RUTO']/newdf['TOTAL VALID VOTES']*100, MWAURE_PERCENT =newdf['MWAURE']/newdf['TOTAL VALID VOTES']*100, WAJACKOYA_PERCENT =newdf['WAJACKOYA']/newdf['TOTAL VALID VOTES']*100)
-
-
-# In[459]:
 
 
 ammended_df.head()
 
 
-# In[460]:
-
-
 ammended_df.columns
-
-
-# In[461]:
 
 
 candidate_percentage = ammended_df[['COUNTY','RAILA_PERCENT', 'RUTO_PERCENT', 'MWAURE_PERCENT', 'WAJACKOYA_PERCENT']]
 
 
-# In[462]:
-
-
 round(candidate_percentage, 2).head()
-
-
-# In[463]:
 
 
 labels = 'RAILA_PERCENT', 'RUTO_PERCENT', 'MWAURE_PERCENT', 'WAJACKOYA_PERCENT'
@@ -73,13 +39,7 @@ plt.legend(labels, loc="center left", prop={'size': 8})
 plt.show()
 
 
-# In[464]:
-
-
 newdf.loc[50,"RAILA":"WAJACKOYA"]
-
-
-# In[465]:
 
 
 def addlabels(xvalue,yvalue):
@@ -99,16 +59,11 @@ if __name__ == '__main__':
     plt.show()
 
 
-# In[453]:
-
-
 top_two_aspirants = candidate_percentage.dropna()
 top_two_candidates = top_two_aspirants.drop(index = 50)
 top_two_candidates[['COUNTY','RAILA_PERCENT', 'RUTO_PERCENT']]
 top_two_candidates.head()
 
-
-# In[441]:
 
 
 candidate_1 = np.array(top_two_candidates['RAILA_PERCENT'])
@@ -118,14 +73,8 @@ cand_2 = candidate_2.tolist()
 county_name = (top_two_candidates['COUNTY'])
 
 
-# In[442]:
-
-
 index = top_two_candidates.index
 index
-
-
-# In[449]:
 
 
 fig = plt.figure(figsize=(25,10), dpi = 200) 
@@ -137,9 +86,6 @@ plt.ylabel('percentage votes', fontsize = 20)
 plt.title("Kenya Presidential Election Tallying Per County")
 plt.xticks(index, county_name, rotation = "vertical")
 plt.show()
-
-
-# In[450]:
 
 
 list = []
@@ -179,16 +125,11 @@ for items in cand_2:
         cand_2_over_75 = len(list_5)
 
 
-# In[451]:
-
 
 percentage = ['25%', '50%', '75%']
 cand_1_tally = [cand_1_over_25, cand_1_over_50, cand_1_over_75]
 cand_2_tally = [cand_2_over_25,cand_2_over_50, cand_2_over_75]
 labels_1 = ['over_25%', 'over_50%', 'over_75%']
-
-
-# In[452]:
 
 
 fig = plt.figure(figsize=(20,10), dpi = 200) 
@@ -201,8 +142,6 @@ plt.title("Comparison of % of votes in counties")
 plt.xticks([0,1,2], labels_1)
 plt.show()
 
-
-# In[ ]:
 
 
 
